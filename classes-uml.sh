82fbc7b54ca01c5f5f262900fb770b3d47496297
Nikita Gladkikh <ngladkikh@anaconda.com>
2022-02-23 08:25:46 +0200

    Fixes imports graph builder, removes dot files

--- classes-uml.sh
+++ classes-uml.sh
@@ -3,15 +3,19 @@
 
 BASE="./venv/bin"
 
+mkdir -p output
+
 $BASE/python -m pylint -ry \
     --disable=all \
     --enable=imports,RP0402 \
     --import-graph=./output/imports_all.dot \
     --ext-import-graph=./output/imports_ext.dot \
     --int-import-graph=./output/imports_int.dot \
-    "$1" && \
+    "$1"
+
 dot -Tpng ./output/imports_int.dot -y -o ./output/imports_int.png
 
 $BASE/pyreverse -d output --colorized -k "$1" && \
-dot -Tpng ./output/classes.dot -y -o ./output/classes.png &&\
-dot -Tpng ./output/packages.dot -y -o ./output/packages.png
+dot -Tpng ./output/classes.dot -y -o ./output/classes.png
+
+rm -r output/*.dot
